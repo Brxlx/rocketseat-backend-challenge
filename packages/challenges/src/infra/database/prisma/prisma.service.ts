d5c9d302d4d465972d61d7da0ab049326ac69ca8
Bruno Santos <brcesar19@gmail.com>
2024-12-03 17:31:52 -0300

    chore(prisma): add prisma and configure database on infra layer

---
+++ packages/challenges/src/infra/database/prisma/prisma.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient<Prisma.PrismaClientOptions, Prisma.LogLevel>
+  implements OnModuleInit, OnModuleDestroy
+{
+  private readonly logger = new Logger(PrismaService.name);
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'event',
+          level: 'query',
+        },
+        {
+          emit: 'event',
+          level: 'info',
+        },
+        {
+          emit: 'event',
+          level: 'warn',
+        },
+        {
+          emit: 'event',
+          level: 'error',
+        },
+      ],
+      errorFormat: 'pretty',
+    });
+  }
+  onModuleInit() {
+    this.$connect();
+    this.$on('query', (event) => {
+      const magenta = '\u001b[35m';
+      this.logger.log(
+        `Query: ${magenta} ${event.query}`,
+        `Params: ${magenta} ${event.query}`,
+        `Duration: ${magenta} ${event.duration} ms`,
+        '---------------------------------',
+      );
+    });
+
+    this.$on('info', (event) => {
+      this.logger.log(`INFO: ${event.message}`);
+    });
+
+    this.$on('warn', (event) => {
+      this.logger.log(`WARN: ${event.message}`);
+    });
+
+    this.$on('error', (event) => {
+      this.logger.log(`ERROR: ${event.message}`);
+    });
+  }
+  onModuleDestroy() {
+    return this.$disconnect();
+  }
+}
