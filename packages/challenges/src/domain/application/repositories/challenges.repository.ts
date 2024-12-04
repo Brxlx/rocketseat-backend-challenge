ddd221b7e92203561f4675d11ddd5cd76cfada3d
Bruno Santos <brcesar19@gmail.com>
2024-12-04 17:47:09 -0300

    chore: update domain challenge repository returns

--- packages/challenges/src/domain/application/repositories/challenges.repository.ts
+++ packages/challenges/src/domain/application/repositories/challenges.repository.ts
@@ -5,10 +5,10 @@ export abstract class ChallengesRepository {
   abstract findById(id: string): Promise<Challenge | null>;
   abstract findByTitle(title: string): Promise<Challenge | null>;
   abstract findManyByTitleOrDescription(
-    titleOrDescription: string,
-    params: PaginationParams,
-  ): Promise<Challenge[]>;
+    titleOrDescription?: string,
+    params?: PaginationParams,
+  ): Promise<{ challenges: Challenge[]; total: number; page: number; itemsPerPage: number }>;
   abstract create(challenge: Challenge): Promise<Challenge>;
   abstract update(challenge: Challenge): Promise<Challenge>;
-  abstract deleteById(challenge: Challenge): Promise<void>;
+  abstract deleteById(id: string): Promise<void>;
 }
