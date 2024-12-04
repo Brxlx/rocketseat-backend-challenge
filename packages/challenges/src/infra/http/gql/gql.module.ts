22b265e0a7386188ef2970cb9fad6d86b67d779a
Bruno Santos <brcesar19@gmail.com>
2024-12-04 17:51:18 -0300

    refactor: move modules folder to gql folder

--- packages/challenges/src/infra/http/gql/gql.module.ts
+++ packages/challenges/src/infra/http/gql/gql.module.ts
@@ -4,11 +4,11 @@ import { DatabaseModule } from '@/infra/database/database.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ChallengeModule } from '../modules/Challenge/challenge.module';
+import { ChallengeModule } from './modules/Challenge/challenge.module';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 import { GraphQLFormatErrorFilter } from '@/infra/filters/GqlFormatError.filter';
-import { UUIDResolver } from '../modules/Challenge/resolvers/uuid-scalar.resolver';
-import { ChallengeResolver } from '../modules/Challenge/resolvers/challenge.resolver';
+import { UUIDResolver } from './modules/Challenge/resolvers/uuid-scalar.resolver';
+import { ChallengeResolver } from './modules/Challenge/resolvers/challenge.resolver';
 
 @Module({
   imports: [
