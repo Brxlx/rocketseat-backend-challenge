f7e65b5dc1cf0d13ba02ff4b4a60bf91474aa72f
Bruno Santos <brcesar19@gmail.com>
2024-12-05 22:49:30 -0300

    feat(messaging)!: add messaging logic
    - add messaging repository
    - add kafka message provider
    - send message to corrections
    - update data in answers db

--- packages/challenges/src/infra/http/gql/modules/Answer/answer.module.ts
+++ packages/challenges/src/infra/http/gql/modules/Answer/answer.module.ts
@@ -1,10 +1,11 @@
 import { FetchAnswersUseCase } from '@/domain/application/use-cases/Answer/fetch-answers-use.case';
 import { SubmitAnswerUseCase } from '@/domain/application/use-cases/Answer/submit-answer-use-case';
 import { DatabaseModule } from '@/infra/database/database.module';
+import { MessagingModule } from '@/infra/Messaging/messaging.module';
 import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, MessagingModule],
   providers: [FetchAnswersUseCase, SubmitAnswerUseCase],
   exports: [FetchAnswersUseCase, SubmitAnswerUseCase],
 })
